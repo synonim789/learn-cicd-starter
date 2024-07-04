8c9d328708e07ca9f0277495eafc9f0207795200
Oskar Fultowicz <synonim789@wp.pl>
2024-07-04 13:02:43 +0200

    add test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name        string
+		headers     http.Header
+		expectedKey string
+		expectedErr error
+	}{
+		{
+			name:        "No Authorization header",
+			headers:     http.Header{},
+			expectedKey: "",
+			expectedErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization header",
+			headers: http.Header{
+				"Authorization": []string{"Bearer somekey"},
+			},
+			expectedKey: "",
+			expectedErr: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Valid Authorization header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey someapikey"},
+			},
+			expectedKey: "someapikey",
+			expectedErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key, err := GetAPIKey(tt.headers)
+			if key != tt.expectedKey {
+				t.Errorf("expected key %v, got %v", tt.expectedKey, key)
+			}
+			if !errors.Is(err, tt.expectedErr) {
+				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
+			}
+		})
+	}
+}
